258f8780e2c0a8ac0e7ef6b5b8160761386f7cbc
Vandriele Barbosa <vandriele.gomes@hotmail.com>
2022-03-25 10:14:20 -0300

    configuração inicial express
    configuração do express

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Hello World!");
+
+});
+
+app.get('/contato', (req, res) => {
+    res.send("Agradecemos o contato");
+
+});
+
+app.listen(3000, () => {
+    console.log("Acessar http://localhost:3000");
+    console.log("Servidor executando");
+});
